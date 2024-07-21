1f36a50f4cf5731a15feda25cc554871512f7a80
Jordy Camacho <jordycamacho225@gmail.com>
2024-07-21 15:25:45 +0200

    creación del proyecto

---
+++ src/main/java/com/example/kunturtatto/KunturtattoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kunturtatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunturtattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunturtattoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kunturtatto/KunturtattoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kunturtatto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunturtattoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
