a8459f5670977900c6e9d455b63edc499b2fb11a
Jordy Camacho <jordycamacho225@gmail.com>
2025-05-03 14:31:30 +0200

    feat: add EmailAlreadyExistsException and ResourceNotFoundException; remove UserNotFoundException

---
+++ src/main/java/com/example/kunturtatto/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.kunturtatto.exception;
+
+public class EmailAlreadyExistsException extends Exception {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/kunturtatto/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.kunturtatto.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
