a1260c06c7f123970e45cdd5a25c1697090415b1
Jordy Camacho <jordycamacho225@gmail.com>
2025-05-15 15:13:51 +0200

    fix: clean up MailConfiguration for improved readability and maintainability

--- src/main/java/com/example/kunturtatto/config/MailConfiguration.java
+++ src/main/java/com/example/kunturtatto/config/MailConfiguration.java
@@ -11,17 +11,16 @@ import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class MailConfiguration {
-    
+
     @Value("${email.sender}")
     private String myEmail;
     @Value("${email.password}")
     private String myPasswordEmail;
-    @Bean
-    public JavaMailSender getJavaMailSender(){
-        
 
+    @Bean
+    public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        
+
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername(myEmail);
@@ -31,8 +30,12 @@ public class MailConfiguration {
 
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable","true");
-        props.put("mail.debug", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.localhost", "kunturtattoo.com");
+        props.put("mail.smtp.helo", "kunturtattoo.com");
 
         return mailSender;
     }
